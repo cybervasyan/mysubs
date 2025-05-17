a609a954ca83b6bb8b37f9313e7078ff0f34100b
cybervasyan <esaydashev@baltbet.ru>
2025-05-17 19:53:50 +0300

    - поправил /next

--- main.go
+++ main.go
@@ -2,21 +2,15 @@ package main
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/joho/godotenv"
-	"log"
 	tg "mysub/bot"
 	"mysub/internal/storage"
 	"os"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	token := os.Getenv("TELEGRAM_APITOKEN")
 	dsn := os.Getenv("DATABASE_URL")
-	err = storage.InitDb(dsn)
+	err := storage.InitDb(dsn)
 
 	var bot *tgbotapi.BotAPI
 	bot, err = tgbotapi.NewBotAPI(token)
